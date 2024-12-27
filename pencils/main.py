493d127f704cd261872e36ca142a2d44b3a51ea6
Danny123456111 <shornikovdaniil111@gmail.com>
2024-12-27 07:55:39 -0600

    fix

--- pencils/main.py
+++ pencils/main.py
@@ -2,19 +2,15 @@ import cv2
 import numpy as np
 chet=[0 for i in range(1,13)]
 for i in range(0,12):
+    chet[i] = 0
     image = cv2.imread(f"images/img ({i+1}).jpg")
-    gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    ret,thresh = cv2.threshold(gray,10,255,cv2.THRESH_BINARY)
-
-    contours,_ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    chet[i]=0
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cactus_coords = []
     for contour in contours:
         area = cv2.contourArea(contour)
-        if area > 1000:  # Если площадь больше 100
+        if area > 1000:
             x, y, w, h = cv2.boundingRect(contour)
 
             rect = cv2.minAreaRect(contour)
