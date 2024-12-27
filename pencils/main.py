d7ae689650539608ecf99d6de836deead517a8ee
Danny123456111 <shornikovdaniil111@gmail.com>
2024-12-27 07:58:14 -0600

    fix2

--- pencils/main.py
+++ pencils/main.py
@@ -7,7 +7,6 @@ for i in range(0,12):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cactus_coords = []
     for contour in contours:
         area = cv2.contourArea(contour)
         if area > 1000:
